d8112e2afa657a00820cf2d616ad39b4830a0c83
Francisco Pereira <fran.p.s@hotmail.com>
2022-08-31 09:19:34 -0300

    feat: add in user repository the method findOneByLogin

--- src/user/user.repository.ts
+++ src/user/user.repository.ts
@@ -19,6 +19,10 @@ export class UserRepository {
     return this.prisma.user.findFirstOrThrow({ where: { id } });
   }
 
+  async findOneByLogin(login: string) {
+    return this.prisma.user.findFirstOrThrow({ where: { login } });
+  }
+
   async update(id: string, data: UpdateUserDto) {
     await this.prisma.user.update({ where: { id }, data });
   }
