47ae8abb7f498007474c984dbb6c4069060fbfc7
Francisco Pereira <fran.p.s@hotmail.com>
2023-01-17 00:16:21 -0300

    fix: add reports feature to app module

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,32 +1,34 @@
 import { MiddlewareConsumer, Module, RequestMethod } from '@nestjs/common';
-import { DeviceModule } from './device/device.module';
-import { ApplierModule } from './applier/applier.module';
-import { UserModule } from './user/user.module';
-import { QuestionnaireModule } from './questionnaire/questionnaire.module';
-import { QuestionModule } from './question/question.module';
-import { AnswerOptionModule } from './answer-option/answer-option.module';
-import { QuestionnaireDataModule } from './questionnaire-data/questionnaire-data.module';
-import { AnswerModule } from './answer/answer.module';
-import { LoginModule } from './login/login.module';
-import { BearerAuthorizationMiddleware } from './middlewares/bearer-authorization-middleware';
-import { QuestionnaireController } from './questionnaire/questionnaire.controller';
-import { QuestionnaireDataController } from './questionnaire-data/questionnaire-data.controller';
-import { QuestionController } from './question/question.controller';
-import { AnswerOptionController } from './answer-option/answer-option.controller';
-import { AnswerController } from './answer/answer.controller';
-import { ApplierController } from './applier/applier.controller';
-import { DeviceController } from './device/device.controller';
-import { UserRepository } from './user/user.repository';
-import { PrismaService } from './prisma.service';
-import { BasicAuthorizationMiddleware } from './middlewares/basic-authorization-middleware';
-import { ApplierRepository } from './applier/applier.repository';
-import { DeviceRepository } from './device/device.repository';
-import { CheckAuthMiddleware } from './middlewares/check-auth-error-middleware';
-import { MeController } from './user/me.controller';
-import { AudioUploadController } from './audio-upload/audio-upload.controller';
 import { ServeStaticModule } from '@nestjs/serve-static';
 import { join } from 'path';
+import { AnswerOptionController } from './answer-option/answer-option.controller';
+import { AnswerOptionModule } from './answer-option/answer-option.module';
+import { AnswerController } from './answer/answer.controller';
+import { AnswerModule } from './answer/answer.module';
+import { ApplierController } from './applier/applier.controller';
+import { ApplierModule } from './applier/applier.module';
+import { ApplierRepository } from './applier/applier.repository';
+import { AudioUploadController } from './audio-upload/audio-upload.controller';
+import { DeviceController } from './device/device.controller';
+import { DeviceModule } from './device/device.module';
+import { DeviceRepository } from './device/device.repository';
 import { HealthCheckController } from './health-check/health-check.controller';
+import { LoginModule } from './login/login.module';
+import { BasicAuthorizationMiddleware } from './middlewares/basic-authorization-middleware';
+import { BearerAuthorizationMiddleware } from './middlewares/bearer-authorization-middleware';
+import { CheckAuthMiddleware } from './middlewares/check-auth-error-middleware';
+import { PrismaService } from './prisma.service';
+import { QuestionController } from './question/question.controller';
+import { QuestionModule } from './question/question.module';
+import { QuestionnaireDataController } from './questionnaire-data/questionnaire-data.controller';
+import { QuestionnaireDataModule } from './questionnaire-data/questionnaire-data.module';
+import { QuestionnaireController } from './questionnaire/questionnaire.controller';
+import { QuestionnaireModule } from './questionnaire/questionnaire.module';
+import { ReportsController } from './reports/reports.controller';
+import { ReportsModule } from './reports/reports.module';
+import { MeController } from './user/me.controller';
+import { UserModule } from './user/user.module';
+import { UserRepository } from './user/user.repository';
 
 @Module({
   imports: [
@@ -42,6 +44,7 @@ import { HealthCheckController } from './health-check/health-check.controller';
     QuestionnaireDataModule,
     AnswerModule,
     LoginModule,
+    ReportsModule,
   ],
   providers: [
     PrismaService,
@@ -67,6 +70,7 @@ export class AppModule {
         { path: 'api/devices', method: RequestMethod.GET },
       )
       .forRoutes(
+        ReportsController,
         QuestionnaireController,
         QuestionnaireDataController,
         QuestionController,
