fcf7a3f9d7e39f159f11b56dc5f9f3d487d27a58
Francisco Pereira dos Santos (Altran) <fran.p.s@hotmail.com>
2023-02-20 11:12:13 -0300

    feat: create a multiple questionnaire data register endpoint
    It allows the possibility of doing the responses register by only one request to the service

--- src/questionnaire-data/questionnaire-data.module.ts
+++ src/questionnaire-data/questionnaire-data.module.ts
@@ -3,6 +3,8 @@ import { QuestionnaireDataService } from './questionnaire-data.service';
 import { QuestionnaireDataController } from './questionnaire-data.controller';
 import { QuestionnaireDataRepository } from './questionnaire-data.repository';
 import { PrismaService } from '../prisma.service';
+import { AnswerService } from '../answer/answer.service';
+import { AnswerRepository } from '../answer/answer.repository';
 
 @Module({
   controllers: [QuestionnaireDataController],
@@ -10,6 +12,8 @@ import { PrismaService } from '../prisma.service';
     PrismaService,
     QuestionnaireDataService,
     QuestionnaireDataRepository,
+    AnswerService,
+    AnswerRepository
   ],
 })
 export class QuestionnaireDataModule {}
